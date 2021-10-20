ffbd14c19f10bd11d8db9f8effb06951313b813a
Atal Prateek Barla <prateek.atal@gmail.com>
2021-10-20 10:07:30 +0530

    js

--- application/views/website/index.php
+++ application/views/website/index.php
@@ -752,15 +752,15 @@
             </ul>
         </div>
     </nav>
-    <script src="plugins/jquery.min.js"></script>
-    <script src="plugins/popper.min.js"></script>
-    <script src="plugins/bootstrap/js/bootstrap.min.js"></script>
-    <script src="plugins/owl-carousel/owl.carousel.min.js"></script>
-    <script src="plugins/jquery.matchHeight-min.js"></script>
-    <script src="plugins/jquery-bar-rating/dist/jquery.barrating.min.js"></script>
-    <script src="plugins/select2/dist/js/select2.min.js"></script>
-    <script src="plugins/slick/slick.js"></script>
-    <script src="plugins/lightGallery/dist/js/lightgallery-all.min.js"></script>
-    <script src="js/main.js"></script>
+    <script src="<?= file_url('assets/plugins/jquery.min.js'); ?>"></script>
+    <script src="<?= file_url('assets/plugins/popper.min.js'); ?>"></script>
+    <script src="<?= file_url('assets/plugins/bootstrap/js/bootstrap.min.js'); ?>"></script>
+    <script src="<?= file_url('assets/plugins/owl-carousel/owl.carousel.min.js'); ?>"></script>
+    <script src="<?= file_url('assets/plugins/jquery.matchHeight-min.js'); ?>"></script>
+    <script src="<?= file_url('assets/plugins/jquery-bar-rating/dist/jquery.barrating.min.js'); ?>"></script>
+    <script src="<?= file_url('assets/plugins/select2/dist/js/select2.min.js'); ?>"></script>
+    <script src="<?= file_url('assets/plugins/slick/slick.js'); ?>"></script>
+    <script src="<?= file_url('assets/plugins/lightGallery/dist/js/lightgallery-all.min.js'); ?>"></script>
+    <script src="<?= file_url('assets/js/main.js'); ?>"></script>
 </body>
 </html>
